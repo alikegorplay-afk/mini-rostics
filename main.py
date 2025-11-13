c7efc4714b893f06ae31a966f975a32a7de34a64
alik <alikegroplay@gmail.com>
2025-11-14 00:22:39 +0900

    AddDocker

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import asyncio
+import uvicorn
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from src.bot.bot import main as bot_main
 from src.core.models import Base
+from src.app.main import create_app
+
+async def run_fastapi(app):
+    """Запуск FastAPI через uvicorn"""
+    config = uvicorn.Config(app)
+    server = uvicorn.Server(config)
+    await server.serve()
 
 async def main():
     engine = create_async_engine('sqlite+aiosqlite:///db.db')
@@ -9,11 +17,21 @@ async def main():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
     
-    Session = async_sessionmaker(engine)
+    Session = async_sessionmaker(engine, expire_on_commit=False)
+    session = Session()
     
-    async with Session() as session:
-        await bot_main(session)
-
+    try:
+        app = create_app(session)
+        
+        if asyncio.iscoroutine(app):
+            app = await app
+        
+        await asyncio.gather(
+            run_fastapi(app),
+            bot_main(session)
+        )
+    finally:
+        await session.close()
 
 if __name__ == "__main__":
     asyncio.run(main())
